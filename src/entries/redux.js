1f723929b6f8c190b44acdb9e8e83a644d138644
alammmunoz <me@alanmunozlopez.com>
2018-02-23 18:56:47 -0300

    {AM} print the state on the screen

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,11 +1,50 @@
+import {
+  createStore
+} from 'redux';
+
+
 const $form = document.getElementById('form');
 if ($form) {
-    $form.addEventListener('submit', handleSubmit);
+  $form.addEventListener('submit', handleSubmit);
 }
 
-function handleSubmit (event) {
-    event.preventDefault();
-    const data = new FormData($form);
-    const title = data.get('title');
-    console.log(title);
+function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData($form);
+  const title = data.get('title');
+  console.log(title);
 }
+
+// const store = createStore(
+//     reducer,
+//     initialState,
+//     enhancer
+// )
+
+const initialState = [
+  {
+    "title": "Despacito"
+  },
+  {
+    "title": "One more thing"
+  },
+  {
+    "title": "Echame la culpa"
+  }
+]
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const $container = document.getElementById('playlist');
+const playlist = store.getState();
+playlist.forEach((item) => {
+  const template = document.createElement('p');
+  template.textContent = item.title;
+  $container.appendChild(template);
+})
+
+console.log(store.getState())
